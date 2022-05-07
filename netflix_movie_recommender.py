5e20c2e975c61e7c446bd5eeb1ce6de6b49209f5
Patrick Gakuo <gakuo.muriuki@gmail.com>
2022-05-07 02:16:54 +0200

    dim-sum

--- netflix_movie_recommender.py
+++ netflix_movie_recommender.py
@@ -1,12 +1,14 @@
+from termios import B110
 import numpy as np
-from scipy.sparse import csr_matrix
+from scipy.sparse import csr_matrix, spdiags
 import multiprocessing
-
-'''
+from itertools import combinations
+import random
+''' ----------------------------------------------------------------------------------------------------------------------------------------------------
 TASK 1 Read the movie ratings files into a sparse matrix 
 '''
-NUMBER_OF_FILES_TO_READ = 1
-movie_text_files = ['netflix_dataset/combined_data_{}.txt'.format(i) for i in  range(1,NUMBER_OF_FILES_TO_READ + 1)]
+NUMBER_OF_FILES_TO_READ = 0
+movie_text_files = ['netflix_dataset/combined_data_{}.txt'.format(i) for i in  range(0,NUMBER_OF_FILES_TO_READ + 1)]
 
 # the movie_ids_list, users_ids_list, and users_ratings_list have the same length and their contents correspond 
 movie_ids_list = [] # a list of movie ids 
@@ -68,7 +70,7 @@ for lst in users_ids_list:
     unrated_users = list(unique_users - set(lst))
         
     # update the users for each movie to include all users who have not yet rated the movie
-    users_ids_list[index] = users_ids_list[index] + unrated_users
+    users_ids_list[index] = lst + unrated_users
 
     # fill out the entries of users who did not rate a movie with zero 
     users_ratings_list[index] = users_ratings_list[index] + np.zeros_like(unrated_users).tolist()
@@ -84,14 +86,62 @@ print("short_wide_sparse_matrix shape: ",  short_wide_sparse_matrix.shape)
 # movies as columns, users as rows 
 tall_thin_sparse_matrix = short_wide_sparse_matrix.transpose()
 print("tall_thin_sparse_matrix shape: ",  tall_thin_sparse_matrix.shape)
-'''
+
+''' ----------------------------------------------------------------------------------------------------------------------------------------------------
 TASK 2: DimSum
 '''
+m,n = tall_thin_sparse_matrix.shape
+B = np.zeros((n,n))
 
-'''
+gamma = 3 # sampling parameter 
+rows = [tall_thin_sparse_matrix.getrow(i) for i in range(0,m) ]
+
+# compute the l2-norms of the columns
+columns = [tall_thin_sparse_matrix.getcol(i) for i in range(0,n)]
+column_l2_norms = [np.sqrt(column.power(2).sum()) for column in columns]
+D  = spdiags(column_l2_norms, 0, len(column_l2_norms), len(column_l2_norms))
+
+
+del columns
+
+# mapper 
+key_value_pairs = {} # output values of the for j,k -> aij * aik
+for rowi in rows:
+    for j,k in combinations(range(0,n), 2): 
+        sampling_probability = gamma * (1 / (column_l2_norms[j]* column_l2_norms[k]))
+        sampling_probability = sampling_probability if (sampling_probability < 1) else 1
+
+        if random.random() < sampling_probability:
+            output = rowi[0,j] * rowi[0,k] # each row of the sparse matrx is indexed as (0, i)
+            if output > 0: # only output values greater than 0
+                if (j,k) in key_value_pairs:
+                    values = key_value_pairs[(j,k)]
+                    values.append(output)
+                    key_value_pairs[(j,k)] = values
+                else:
+                    key_value_pairs[(j,k)] = [output]
+
+
+#reducer 
+for (j,k), values in key_value_pairs.items():
+    denom = (column_l2_norms[j] * column_l2_norms[k])
+    sampling_probability = gamma * (1 / denom)
+    if sampling_probability > 1:
+        B[j,k] = sum(values)/denom # normalise where probability was greater than 1
+
+    else:
+         B[j,k] =  sum(values)/gamma # estimate the expectation where probability was less than 1
+
+print('B', B)
+A_transpose_A = D @ B @ D
+
+print('A_transpose_A', A_transpose_A)
+
+
+''' ----------------------------------------------------------------------------------------------------------------------------------------------------
 TASK 3: Gradient Descent Computation
 '''
 
-'''
+''' ----------------------------------------------------------------------------------------------------------------------------------------------------
 TASK 4: Accuracy calculation
 '''
